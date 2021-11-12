313a07cd6ef0acdff45d24066cd1e37041abfb47
MoonRaySpex <rachel@myemail.com>
2021-11-11 19:12:59 -0500

    number

--- cat_names.py
+++ cat_names.py
@@ -12,7 +12,7 @@ award_titles = ["GC", "RW", "CH"]
 def select_from_array(array):
   return random.choice(array)
     
-for x in range(10):
+for x in range(5):
 
   title = select_from_array(award_titles)
   # print(title)
